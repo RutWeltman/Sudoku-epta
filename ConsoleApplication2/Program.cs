1fe7a05f3678585f5c3d514c18f054d73931b1d3
Rut <weltmanrut@gmail.com>
2025-05-25 23:37:47 +0300

    я звезда

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -2,6 +2,7 @@
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security.AccessControl;
 using System.Security.Cryptography;
@@ -13,7 +14,7 @@ namespace ConsoleApplication2
     {
         private int[,] board =
         {
-            { 9, 8, 7, 6, 5, 4, 3, 2, 1 },
+            { 9, 8, 7, 6, 5, 4, 3, 2, 0 },
             { 2, 4, 6, 1, 7, 3, 9, 8, 5 },
             { 3, 5, 1, 9, 2, 8, 7, 4, 6 },
             { 1, 2, 8, 5, 3, 7, 6, 9, 4 },
@@ -24,15 +25,31 @@ namespace ConsoleApplication2
             { 8, 6, 3, 7, 4, 5, 2, 1, 9 }
         };
 
-        public void SetValue(int row, int col, int value)
+        int mouseX = 0;
+        int mouseY = 0;
+
+        public void SetPosition(int x, int y)
         {
-            board[row, col] = value;
+            if (x >= 0 && x <= 8)
+            {
+                this.mouseX = x;
+            }
+
+            if (y >= 0 && y <= 8)
+            {
+                this.mouseY = y;
+            }
         }
 
-        public int GetValue(int row, int col)
+        public int GetX()
         {
-            return board[row, col];
+            return this.mouseX;
         }
+        public int GetY()
+        {
+            return this.mouseY;
+        }
+        
 
 
         public bool IsValid()
@@ -83,30 +100,88 @@ namespace ConsoleApplication2
                         Console.Write("| ");
 
                     int value = board[row, col];
-                    Console.Write(value == 0 ? ". " : value + " ");
+                    if (this.mouseX == col && this.mouseY == row)
+                    {
+                        Console.Write(value == 0 ? "\u2b1c\ufe0f<" : value + "<");
+                    }
+                    else { Console.Write(value == 0 ? "\u2b1c\ufe0f " : value + " ");}
                 }
-               Console.WriteLine();
+
+                Console.WriteLine();
             }
         }
 
-    public class Program
+        public void SetValue(int z)
         {
-            public static void Main(string[] args)
-            {
-                Sudoku sudoku = new Sudoku();
-                bool valid = sudoku.IsValid();
-
-
-                Console.WriteLine(valid);
-                sudoku.PrintBoard();
-            }
+            this.board[this.mouseY, this.mouseX] = z;
         }
     }
-    }
-             
-           
-
-            
-                
-            
     
+    
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Sudoku sudoku = new Sudoku();
+            while (sudoku.IsValid() != true)
+            {
+                
+                sudoku.PrintBoard();
+                ConsoleKey key = Console.ReadKey().Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        sudoku.SetPosition(sudoku.GetX(), sudoku.GetY()-1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        sudoku.SetPosition(sudoku.GetX(), sudoku.GetY()+1);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        sudoku.SetPosition(sudoku.GetX()-1, sudoku.GetY());
+                        break;
+                    case ConsoleKey.RightArrow:
+                       sudoku.SetPosition(sudoku.GetX()+1, sudoku.GetY());
+                        break;
+                    
+                    case ConsoleKey.D0:
+                        sudoku.SetValue(0);
+                        break;
+                    case ConsoleKey.D1:
+                        sudoku.SetValue(1);
+                        break;
+                    case ConsoleKey.D2:
+                        sudoku.SetValue(2);
+                        break;
+                    case ConsoleKey.D3:
+                        sudoku.SetValue(3);
+                        break;
+                    case ConsoleKey.D4:
+                        sudoku.SetValue(4);
+                        break;
+                    case ConsoleKey.D5:
+                        sudoku.SetValue(5);
+                        break;
+                    case ConsoleKey.D6:
+                        sudoku.SetValue(6);
+                        break;
+                    case ConsoleKey.D7:
+                        sudoku.SetValue(7);
+                        break;
+                    case ConsoleKey.D8:
+                        sudoku.SetValue(8);
+                        break;
+                    case ConsoleKey.D9:
+                        sudoku.SetValue(9);
+                        break;
+                }
+                Console.Clear();
+                
+
+            }
+            Console.WriteLine("ты победил");
+        }
+    }
+}
+
